d16a78fe8b57534e14f60d9adab2f83933e4ad5e
Dung246 <dungoo2605@gmail.com>
2026-04-07 16:03:43 +0700

    Update b2.java

--- src/b2/b2.java
+++ src/b2/b2.java
@@ -1,17 +1,4 @@
 package b2;
 
-//@Component
-//@Scope("prototype")
-//public class PlaySession {
-//    private double playTime = 0;
-//
-//    public void addTime(double time) {
-//        this.playTime += time;
-//    }
-//
-//    public double getPlayTime() {
-//        return playTime;
-//    }
-//}
-
 //Trong Spring, Bean mặc định có scope là Singleton, nghĩa là chỉ có một instance duy nhất trong toàn bộ ứng dụng.
+//Khi sử dụng Singleton cho PlaySession (một đối tượng chứa trạng thái riêng của từng người dùng), tất cả các máy trạm sẽ dùng chung một instance, dẫn đến việc dữ liệu thời gian chơi bị chia sẻ và ghi đè lẫn nhau, gây ra lỗi tính tiền sai.
